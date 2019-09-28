96fda8ce1fcbf21a624f372cd3d7b3f36668aa8e
axelDeveloper <axelpatton@gmail.com>
2019-09-27 22:03:32 -0400

    Update cod fonte

--- Importar_scansione.py
+++ Importar_scansione.py
@@ -1,46 +1,146 @@
 
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+import requests
 import sqlite3
 import aiosqlite
 import asyncio
-from utils import Conexao_SQLLITE
-
-
-conn=Conexao_SQLLITE('nome')
+from dataclasses import dataclass
 
 TEST_DB = 'nome.db'
 
-query = "select * from Pessoa_Fisica"
+query = "select * from Pessoa_Fisica where rowid = 1 "
 
-async def fetchall_async(sql):
+lista_pessoa = []
+
+
+@dataclass
+class Pessoa:
+    """
+        :TODO   VO de pessoa a ser Importada
+    """
+    cod: int
+    email: str
+    cpf: int
+    nome_completo: str
+    aniversario: str
+    endereco: str
+    numero_residencia: str
+    complemento: str
+    bairro: str
+    cidade: str
+    telefone_contato: int
+    profissao: str
+    cep: str
+
+
+class ViaCEP:
+
+
+    def __init__(self, cep , cidade=None , endereco=None):
+        self.cep = cep
+        self.cidade =cidade
+        self.endereco = endereco
+
+
+    def getEndereco(self):
+        url_api  = ('http://www.viacep.com.br/ws/MS/%s/' % self.cidade )
+        url_api2 = (url_api + '%s/json' % self.endereco)
+        req = requests.get(url_api2)
+        if req.status_code == 200:
+            dados_json = json.loads(req.text)
+            return dados_json
+
+
+    def getDadosCEP(self):
+        url_api = ('http://www.viacep.com.br/ws/%s/json' % self.cep)
+        req = requests.get(url_api)
+        if req.status_code == 200:
+            dados_json = json.loads(req.text)
+            return dados_json
+
+
+async def fetchall_async(sql, itera=0):
     async with aiosqlite.connect(TEST_DB) as db:
-        #await db.execute('INSERT INTO some_table ...')
-        #await db.commit()
+        # await db.execute('INSERT INTO some_table ...')
+        # await db.commit()
 
         async with db.execute(sql) as cursor:
-            async for row in cursor:
-                print(row[1])
 
- 
+            async for row in cursor:
+                itera += 1
+                #print(  float( row[1])  )
+                #print(  row[0]  )
+
+                pessoa = Pessoa(
+                    cod=itera,
+                    email=row[0],
+                    cpf=row[1],
+                    nome_completo=row[2],
+                    aniversario=row[3],
+                    endereco=row[4],
+                    numero_residencia=row[5],
+                    complemento=row[6],
+                    bairro=row[7],
+                    cidade=row[8],
+                    telefone_contato=row[9],
+                    profissao=row[10],
+                    cep= ""
+                )
+                lista_pessoa.append(pessoa)
+            return lista_pessoa
+
+
 async def fetchall_task():
-    students = await fetchall_async(query)
+    students = await fetchall_async(query, 1)
+    for row in students:
+        # print("Task ", row.cpf)
+        # print ( "\"%s\"  %s (%s)" % (row.cpf, row.cidade, row.endereco) )
+
+        d = ViaCEP("79041080", str(row.cidade), str(row.endereco) )
+        data = d.getEndereco()
+        #row.cep = data
+
+        if row.bairro  in  data[0]['bairro']:
+            print("mesmo bairro")
+
+        row.cep = data[0]['cep']
+        print( row.bairro )
+        print( data[0]['bairro'] )
+        print( data[0]['cep'] )
 
 
 async def All_async(sql):
+    """
+        await fetc all rows
+    """
     db = await aiosqlite.connect(TEST_DB)
     cursor = await db.execute(sql)
     row = await cursor.fetchone()
     rows = await cursor.fetchall()
     await cursor.close()
     await db.close()
-    #return await cursor.fetchall()
+    # return await cursor.fetchall()
     return rows
-    
+
+
 async def All_task():
-    #conn = aiosqlite.connect('nome.db')
     rows = await All_async(query)
-    print( rows )
+    print(rows)
 
 
+async def Consulta_end_async(cep):
+    d = ViaCEP(cep)
+    data = d.getDadosCEP()
+    return data
+
+
+async def Consulta_task():
+    #conn = aiosqlite.connect('nome.db')
+    rows = await Consulta_end_async("79041080")
+    print(rows)
 
 
 """
@@ -48,29 +148,21 @@ async def Crud():
     async with aiosqlite.connect(...) as db:
         await db.execute('INSERT INTO some_table ...')
         await db.commit()
-
         async with db.execute('SELECT * FROM some_table') as cursor:
             async for row in cursor:
-
 """
 
 try:
-    
-    #asyncio.run(some_task()) 
-    
-    #loop.run_forever()   
+
+    # asyncio.run(some_task())
+
+    # loop.run_forever()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(fetchall_task())
-    #loop.run_until_complete(All_task())
-    #loop.run_until_complete(do_update_many())
-    #loop.run_until_complete(use_distinct_command())
+    # loop.run_until_complete(All_task())
+    # loop.run_until_complete(Consulta_task())
+    # loop.run_until_complete(use_distinct_command())
 finally:
     loop.run_until_complete(loop.shutdown_asyncgens())
     loop.close()
-    #pass
-
-
-
-
-
-
+    # pass
--- provider/query.sql
+++ provider/query.sql
@@ -1,21 +1,60 @@
---SELECT * FROM `agua`;
+SELECT * FROM agua where cpf = 306647133;
 SELECT * FROM `violencia`;
 SELECT * FROM `seguranca`;
 
---SELECT count(*) FROM `agua`;
---SELECT count(*) FROM `seminario`;
---SELECT count(*) FROM `seguranca`;
---SELECT count(*) FROM `violencia`;
+SELECT count(*) FROM `agua`;
+SELECT count(*) FROM `seminario`;
+SELECT count(*) FROM `seguranca`;
+SELECT count(*) FROM `violencia`;
 
 
 SELECT count(*) FROM `Pessoa_Fisica`;
 
-SELECT rowid, * FROM Pessoa_Fisica order by cpf , nome_completo ;
+SELECT rowid, * FROM Pessoa_Fisica order by  nome_completo,  cpf, rowid  ASC ;
 
--- SELECT rowid, * FROM tbl1 WHERE letter = 'B'
-SELECT * FROM violencia where nome_completo = 'Nicolas Nichele';
+select 
+     REPLACE(cidade,'/MS','') as cidade
+from Pessoa_Fisica where cpf > 0; 
+
+--Campo Grande/ MS
+
+update Pessoa_Fisica 
+    set cidade =   REPLACE(cidade,'Chapadao do sul','Chapadao do Sul')
+    --set cidade =   'Campo Grande'
+--set cpf =  REPLACE(cpf,'.0','')
+--set nome_completo = 'ALINE BALTA VIANNA'
+where  
+--cidade =  'CSMPO GRANDE';  
+rowid > 0  ;
+
+SELECT rowid, cidade FROM Pessoa_Fisica where  cidade like '%vinhema%' ;
+SELECT rowid, cidade FROM Pessoa_Fisica  order by cidade asc;
+
+SELECT rowid, cidade FROM Pessoa_Fisica where  rowid > 693  ;
+
+SELECT rowid, cidade FROM Pessoa_Fisica where nome_completo = 'WANDA FALEIROS';
+
+SELECT rowid, * FROM Pessoa_Fisica where cpf ='inf.0';
+
+
+
 SELECT * FROM agua where nome_completo = 'Nicolas Nichele';   -- 000.002.251-76
-                                                                         
+     
+--000.000.000-00                                        
+--018.605.389-40
+-- 18.605.389-40
+
+
+select rowid, substr('00000000000'||cpf,-11) as cpf FROM Pessoa_Fisica;
+
+SELECT rowid, substr('00000000000'||cpf,-11) as cpf_fotmat , cpf FROM Pessoa_Fisica where  rowid in(981 ) ;
+                            
+
+SELECT count(*) as tot, cpf  FROM Pessoa_Fisica 
+group by cpf ,nome_completo
+HAVING tot = 1
+order by  tot,  cpf ASC ;
+
 
 
 --INSERT INTO Pessoa_Fisica 
@@ -50,8 +89,8 @@ SELECT
 FROM seminario;
 
 
---delete from Pessoa_Fisica where nome_completo =  '06369384178';
---delete from Pessoa_Fisica where  rowid = 288;
+--delete from Pessoa_Fisica where nome_completo =  '06369384178';--
+--delete from Pessoa_Fisica where  rowid in(981 ) ;
 
 /*
 email             TEXT,
